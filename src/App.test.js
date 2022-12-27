9d25d9471e6702f2eaf234ce839c289d266cbc7d
Your Name <you@example.com>
2022-12-26 20:34:14 -0800

    added codes

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,13 @@
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Catalina Barriga'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('Catalina Barriga')
+  })
 
+  beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
 
